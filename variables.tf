205eccfd6987b5d5a4b5613c32c7725a69e673ba
rickyxie0929 <148598858+rickyxie0929@users.noreply.github.com>
2026-07-17 09:49:16 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
 variable "tenant_project_id" {
   description = "The Tenant Project ID"
   type        = string
+  default     = "ez-agentengine-autopush"
 }
-
 variable "tenant_project_number" {
   description = "The Tenant Project Number"
   type        = string
+  default     = "231044352906"
 }
-
 variable "location" {
   description = "The region (e.g. us-central1)"
   type        = string
